c2261dd42d6a3275facc933820e3ae7a35b6cd21
Kowsikrameshbabu <135093724+Kowsikrameshbabu@users.noreply.github.com>
2024-08-14 07:52:33 +0530

    first commit

--- jwtworking/JobApplicationBackend/YogaAcademy_Backend/demo/src/main/java/com/example/demo/utils/ChangePassword.java
+++
@@ -1,3 +0,0 @@
-package com.example.demo.utils;
-public record ChangePassword(String password, String repeatPassword) {
-}
--- jwtworking/JobApplicationBackend/YogaAcademy_Backend/demo/src/main/java/com/example/demo/utils/RefreshTokenRequest.java
+++
@@ -1,6 +0,0 @@
-package com.example.demo.utils;
-import lombok.Data;
-@Data
-public class RefreshTokenRequest {
-    private String refreshToken;
-}
